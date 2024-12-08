70ead19e8be0bcbfa6b4fc068dfc66f5dee3b358
beshir <youssefahmedbeshir@gmail.com>
2024-12-08 10:19:33 +0200

    Removed unnecessary files

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    //T5x5Backend x5_backend;
-    //engine.rootContext()->setContextProperty("x5_backend", &x5_backend);
-
     engine.loadFromModule("BoardGame", "Main");
     if (engine.rootObjects().isEmpty())
         return -1;
