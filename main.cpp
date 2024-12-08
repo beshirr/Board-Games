c42ab4ef39f2876402fd3620d97b77523c78ebd9
beshir <youssefahmedbeshir@gmail.com>
2024-12-08 07:00:00 +0200

    Main

--- main.cpp
+++ main.cpp
@@ -3,32 +3,6 @@
 #include <QQmlContext>
 #include "t5x5backend.h"
 
-
-// int main(int argc, char *argv[])
-// {
-//     QGuiApplication app(argc, argv);
-//     QQmlApplicationEngine engine;
-//
-//     qmlRegisterType<T5x5Backend>("BoardGames.Backend", 1, 0, "T5x5Backend");
-//
-//
-//     QObject::connect(
-//         &engine,
-//         &QQmlApplicationEngine::objectCreationFailed,
-//         &app,
-//         []() { QCoreApplication::exit(-1); },
-//         Qt::QueuedConnection);
-//
-//
-//     engine.loadFromModule("BoardGame", "Main");
-//     engine.load(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "TicTacToe5x5.qml"));
-//
-//     return app.exec();
-// }
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QQmlContext>
-
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
