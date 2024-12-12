c2faf818d49df91ac753ee565365fcd865c25397
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2024-12-12 21:10:05 +0200

    fixed word tic tac toe logic

--- WordTicTacToe.cpp
+++ WordTicTacToe.cpp
@@ -6,11 +6,12 @@ using namespace std;
 WordBoard::WordBoard(){
     rows = 3;
     columns = 3;
-    board = new char*[rows];
+    WordsFile = "dic.txt";
+    board = new string*[rows];
     for(int i = 0; i < rows; i++) {
-        board[i] = new char[columns];
+        board[i] = new string[columns];
         for(int j = 0; j < columns; j++) {
-            board[i][j] = '-';
+            board[i][j] = "-";
         }
     }
 }
@@ -22,8 +23,8 @@ WordBoard::~WordBoard(){
     delete[] board;
 }
 
-bool WordBoard::update_board(int x, int y, char symbol) {
-    if (x < 0 || y < 0 || x >= rows || y >= columns || board[x][y] != '-')return false;
+bool WordBoard::update_board(int x, int y, string symbol) {
+    if (x < 0 || y < 0 || x >= rows || y >= columns || board[x][y] != "-")return false;
     else {
         board[x][y] = symbol;
         n_moves++;
@@ -42,14 +43,26 @@ void WordBoard::display_board() {
     cout << endl;
 }
 
-bool WordBoard::is_word(){
-
+bool WordBoard::is_word(const string& word) {
+    ifstream file(WordsFile);
+    string line;
+    while(getline(file, line)) {
+        if(line == word) return true;
+    }
+    file.close();
+    return false;
 }
 
 bool WordBoard::is_win() {
     for(int i = 0; i < rows; i++) {
-
+        if (is_word(board[i][0] + board[i][1] + board[i][2])) return true;
     }
+    for(int j = 0; j < columns; j++) {
+        if (is_word(board[0][j] + board[1][j] + board[2][j])) return true;
+    }
+    if (is_word(board[0][0] + board[1][1] + board[2][2])) return true;
+    if (is_word(board[0][2] + board[1][1] + board[2][0])) return true;
+    return false;
 }
 
 bool WordBoard::is_draw() {
