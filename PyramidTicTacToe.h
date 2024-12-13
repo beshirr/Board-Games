33b6b39944af898417917d26344606fd32cd6aa1
Zeyad Mohamed Araft <zeyadarafat833@gmail.com>
2024-12-13 15:55:11 +0200

    merged the header with the implementation in the same file

--- PyramidTicTacToe.h
+++ PyramidTicTacToe.h
@@ -2,21 +2,22 @@
 #define PYRAMIDTICTACTOE_H
 #include "BoardGame_Classes.h"
 #include <iostream>
+#include <QString>
 
-class PyramidBoard : public Board<char>{
+class PyramidBoard : public Board<QString>{
 public:
     PyramidBoard();
     ~PyramidBoard();
-    bool update_board(int x, int y, char symbol) override;
+    bool update_board(int x, int y, QString symbol) override;
     void display_board() override;
     bool is_win() override;
     bool is_draw() override;
     bool game_is_over() override;
 };
 
-class PyramidPlayer: public Player<char>{
+class PyramidPlayer: public Player<QString>{
 public:
-    PyramidPlayer(string playerName, char playerSymbol);
+    PyramidPlayer(string playerName, QString playerSymbol);
     void getmove(int& x, int& y) override;
 };
 
@@ -24,4 +25,82 @@ public:
 
 
 #endif //PYRAMIDTICTACTOE_H
+
+
+
+
+// Implementation ----------------------------------------------------------------
+
+
+PyramidBoard::PyramidBoard(){
+    rows = 3;
+    columns = 5;
+    board = new QString* [rows];
+    for(int i = 0; i < rows; i++) {
+        board[i] = new QString[columns];
+        for(int j = 0; j < columns; j++) {
+            board[i][j] = "-";
+        }
+    }
+    board[0][0] = board[0][1] = board[0][3] = board[0][4] = board[1][0] = board[1][4] = " ";
+}
+
+PyramidBoard::~PyramidBoard() {
+    for(int i = 0; i < rows; i++) {
+        delete[] board[i];
+    }
+    delete[] board;
+}
+
+bool PyramidBoard::update_board(int x, int y, QString symbol) {
+    if (x < 0 || y < 0 || x >= rows || y >= columns || board[x][y] != "-")return false;
+    else {
+        board[x][y] = symbol;
+        n_moves++;
+        return true;
+    }
+}
+
+void PyramidBoard::display_board() {
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < columns; j++) {
+            cout << board[i][j].toStdString() << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool PyramidBoard::is_win() {
+    for (int i = 1; i < rows; ++i) {
+        for (int j = 0; j < columns-2; ++j) {
+            if((board[i][j] == board[i][j+1]) && (board[i][j] == board[i][j+2]) && (board[i][j] != '-'))return true;
+            if ((j == 2) && (i == 1)){
+                if((board[i][j] == board[i+1][j]) && (board[i][j] == board[i-1][j]) && (board[i][j]!= '-')) return true;
+            }
+        }
+    }
+    if ((board[0][2] == board[1][1]) && (board[0][2] == board[2][0]) && (board[0][2] != '-')) return true;
+    else if ((board[0][2] == board[1][3]) && (board[0][2] == board[2][4]) && (board[0][2] != '-')) return true;
+
+    return false;
+}
+
+bool PyramidBoard::is_draw() {
+    if (n_moves == 9) return true;
+    return false;
+}
+
+bool PyramidBoard::game_is_over() {
+    return is_win() || is_draw();
+}
+
+void PyramidPlayer::getmove(int &x, int &y) {
+    cout << name << ", enter your move (x y): ";
+    cin >> x >> y;
+    x--;
+    y--;
+}
+
+PyramidPlayer::PyramidPlayer(string playerName, QString playerSymbol) : Player(playerName, playerSymbol){};
 
--- WordTicTacToe.h
+++ WordTicTacToe.h
@@ -4,28 +4,116 @@
 #include "BoardGame_Classes.h"
 #include <iostream>
 #include <fstream>
+#include <QString>
 
 
-class WordBoard: public Board<string>{
+class WordBoard: public Board<QString>{
 private:
     string WordsFile;
-    bool is_word(const string& word);
+    bool is_word(const QString& word);
 
 public:
     WordBoard();
     ~WordBoard();
-    bool update_board(int x, int y, string symbol) override;
+    bool update_board(int x, int y, QString symbol) override;
     void display_board() override;
     bool is_win() override;
     bool is_draw() override;
     bool game_is_over() override;
 };
 
-class WordPlayer: public Player<char>{
+class WordPlayer: public Player<QString>{
 public:
-    WordPlayer(string playerName, char playerSymbol);
+    WordPlayer();
+    WordPlayer(const QString& playerName, QString playerSymbol);
     void getmove(int& x, int& y) override;
 };
+
+
+// implementation----------------------------------------------------------------
+
+
+using namespace std;
+
+WordBoard::WordBoard(){
+    rows = 3;
+    columns = 3;
+    WordsFile = "dic.txt";
+    board = new QString*[rows];
+    for(int i = 0; i < rows; i++) {
+        board[i] = new QString[columns];
+        for(int j = 0; j < columns; j++) {
+            board[i][j] = "-";
+        }
+    }
+}
+
+WordBoard::~WordBoard(){
+    for(int i = 0; i < rows; i++) {
+        delete[] board[i];
+    }
+    delete[] board;
+}
+
+bool WordBoard::update_board(int x, int y, QString symbol) {
+    if (x < 0 || y < 0 || x >= rows || y >= columns || board[x][y] != "-")return false;
+    else {
+        board[x][y] = symbol;
+        n_moves++;
+        return true;
+    }
+}
+
+void WordBoard::display_board() {
+    for(int i = 0; i < rows; i++) {
+        cout << i+1 << " ";
+        for(int j = 0; j < columns; j++) {
+            cout << board[i][j].toStdString() << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool WordBoard::is_word(const QString& word) {
+    ifstream file(WordsFile);
+    string line;
+    while(getline(file, line)) {
+        if(line == word) return true;
+    }
+    file.close();
+    return false;
+}
+
+bool WordBoard::is_win() {
+    for(int i = 0; i < rows; i++) {
+        if (is_word(board[i][0] + board[i][1] + board[i][2])) return true;
+    }
+    for(int j = 0; j < columns; j++) {
+        if (is_word(board[0][j] + board[1][j] + board[2][j])) return true;
+    }
+    if (is_word(board[0][0] + board[1][1] + board[2][2])) return true;
+    if (is_word(board[0][2] + board[1][1] + board[2][0])) return true;
+    return false;
+}
+
+bool WordBoard::is_draw() {
+    if (n_moves == 9) return true;
+    return false;
+}
+
+bool WordBoard::game_is_over() {
+    return is_win() || is_draw();
+}
+
+WordPlayer::WordPlayer(const QString& playerName, QString playerSymbol): Player(playerName.toStdString(), playerSymbol) {}
+
+void WordPlayer::getmove(int &x, int &y) {
+    cout << name << ", enter your move (x y): ";
+    cin >> x >> y;
+    x--;
+    y--;
+}
 
 
 #endif //WORDTICTACTOE_H
