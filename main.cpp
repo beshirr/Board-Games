7200d595aed5d31e9873becbc6caa4f49d46ffc5
beshir <youssefahmedbeshir@gmail.com>
2024-12-15 16:30:19 +0200

    QML registers for the Misere

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <QQmlContext>
 
 #include "TicTacToe5x5.h"
-#include "PyramidTicTacToe.h"
+#include "Misere.h"
+// #include "PyramidTicTacToe.h"
 
 
 int main(int argc, char *argv[]) {
@@ -13,9 +14,11 @@ int main(int argc, char *argv[]) {
     qmlRegisterType<Board5x5>("TicTacToe5x5", 1, 0, "Board5x5");
     qmlRegisterType<Player5x5>("TicTacToe5x5", 1, 0, "Player5x5");
     qmlRegisterType<RandomPlayer5x5>("TicTacToe5x5", 1, 0, "RandomPlayer5x5");
-    qmlRegisterType<PyramidBoard>("PyramidTicTacToe", 1, 0, "PyramidBoard");
-    qmlRegisterType<PyramidPlayer>("PyramidTicTacToe", 1, 0, "PyramidPlayer");
-
+    // qmlRegisterType<PyramidBoard>("PyramidTicTacToe", 1, 0, "PyramidBoard");
+    // qmlRegisterType<PyramidPlayer>("PyramidTicTacToe", 1, 0, "PyramidPlayer");
+    qmlRegisterType<MisereBoard>("Misere", 1, 0, "MisereBoard");
+    qmlRegisterType<MiserePlayer>("Misere", 1, 0, "MiserePlayer");
+    qmlRegisterType<MisereRandomPlayer>("Misere", 1, 0, "MisereRandomPlayer");
 
     engine.loadFromModule("BoardGame", "Main");
     if (engine.rootObjects().isEmpty())
